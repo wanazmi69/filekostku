0190234bf3dda3bd33081945a8521ef16f869c12
wanazmi69 <lawtheoscar@gmail.com>
2022-07-09 19:51:15 +0900

    test

--- myweb/settings.py
+++ myweb/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tailwind',
     'theme',
-    # 'django_browser_reload',
+    'django_browser_reload',
     'assets',
     'paygate',
     'adminku',
